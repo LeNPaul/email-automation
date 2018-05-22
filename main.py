54081075277058bb982ebe139493a06c0747c63b
Paul Le <l.nguyen.paul@gmail.com>
2018-05-21 23:06:11 -0400

    Read credentials from file

--- main.py
+++ main.py
@@ -1,6 +1,26 @@
 from selenium import webdriver
+import json
+import time
+
+# Read in credentials and chromedriver file path
+file = open('credentials.json', 'r')
+info = json.loads(file.read())
 
 # Set path to the chromedriver
-browser = webdriver.Chrome('/Users/paul.le/Documents/Github/email-automation/chromedriver')
+browser = webdriver.Chrome(info['cdPath'])
 
-browser.get('https://google.ca')
+browser.get('https://accounts.google.com')
+
+time.sleep(1)
+
+email = browser.find_element_by_name('identifier')
+email.send_keys(info['username'])
+submitEmail = browser.find_element_by_id('identifierNext')
+submitEmail.click()
+
+time.sleep(1)
+
+password = browser.find_element_by_name('password')
+password.send_keys(info['password'])
+submitPassword = browser.find_element_by_id('passwordNext')
+submitPassword.click()
