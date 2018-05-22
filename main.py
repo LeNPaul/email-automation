ac761385bf02ec2696e306feba4665e4d8a3512f
Paul Le <l.nguyen.paul@gmail.com>
2018-05-21 21:36:12 -0400

    Add main.py script

---
+++ main.py
@@ -0,0 +1,6 @@
+from selenium import webdriver
+
+# Set path to the chromedriver
+browser = webdriver.Chrome('/Users/paul.le/Documents/Github/email-automation/chromedriver')
+
+browser.get('https://google.ca')
