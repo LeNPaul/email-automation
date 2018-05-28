afeb447edf4a7b398ef69f7ccd00b49ef1b551ad
Paul Le <l.nguyen.paul@gmail.com>
2018-05-28 14:21:03 -0400

    Add function for logging into the Google account

--- main.py
+++ main.py
@@ -2,6 +2,26 @@ from selenium import webdriver
 import json
 import time
 
+def login():
+
+    browser.get('https://accounts.google.com')
+
+    email = browser.find_element_by_name('identifier')
+    email.send_keys(info['username'])
+    submitEmail = browser.find_element_by_id('identifierNext')
+    submitEmail.click()
+
+    time.sleep(1)
+
+    password = browser.find_element_by_name('password')
+    password.send_keys(info['password'])
+    submitPassword = browser.find_element_by_id('passwordNext')
+    submitPassword.click()
+
+    time.sleep(1)
+
+    return browser.find_element_by_link_text('About Google')
+
 # Read in credentials and chromedriver file path
 file = open('credentials.json', 'r')
 info = json.loads(file.read())
@@ -9,32 +29,22 @@ info = json.loads(file.read())
 # Set path to the chromedriver
 browser = webdriver.Chrome(info['cdPath'])
 
-browser.get('https://accounts.google.com')
+# Log into Google account
+loggedIn = None
+while not loggedIn:
+    try:
+        loggedIn = login()
+    except:
+        print "Failed to login - will try again"
 
-time.sleep(1)
+#browser.get(info['linkOne'])
 
-email = browser.find_element_by_name('identifier')
-email.send_keys(info['username'])
-submitEmail = browser.find_element_by_id('identifierNext')
-submitEmail.click()
+#time.sleep(10)
 
-time.sleep(1)
+#browser.get(info['linkTwo'])
 
-password = browser.find_element_by_name('password')
-password.send_keys(info['password'])
-submitPassword = browser.find_element_by_id('passwordNext')
-submitPassword.click()
+#time.sleep(10)
 
-time.sleep(1)
-
-browser.get(info['linkOne'])
-
-time.sleep(10)
-
-browser.get(info['linkTwo'])
-
-time.sleep(10)
-
-browser.get(info['linkThree'])
+#browser.get(info['linkThree'])
 
 # todo: add retry functionality on error
