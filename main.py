3d65542b31ac703ebdc8fad26d93740480592cf6
Paul Le <l.nguyen.paul@gmail.com>
2018-06-07 12:52:11 -0400

    Update main function for correct module reference

--- main.py
+++ main.py
@@ -11,11 +11,11 @@ schedule.every().thursday.at("17:00").do(turnon.enable)
 schedule.every().friday.at("17:00").do(turnon.enable)
 
 # Schedule turning off
-schedule.every().monday.at("9:00").do(turnon.disable)
-schedule.every().tuesday.at("9:00").do(turnon.disable)
-schedule.every().wednesday.at("9:00").do(turnon.disable)
-schedule.every().thursday.at("9:00").do(turnon.disable)
-schedule.every().friday.at("9:00").do(turnon.disable)
+schedule.every().monday.at("9:00").do(turnoff.disable)
+schedule.every().tuesday.at("9:00").do(turnoff.disable)
+schedule.every().wednesday.at("9:00").do(turnoff.disable)
+schedule.every().thursday.at("9:00").do(turnoff.disable)
+schedule.every().friday.at("9:00").do(turnoff.disable)
 
 while True:
     schedule.run_pending()
