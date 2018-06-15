d59cf2a9aa8860d59ec64336e9015d21a26b8c95
Paul Le <l.nguyen.paul@gmail.com>
2018-06-15 18:18:54 -0400

    Add ping function

--- app.py
+++ app.py
@@ -2,9 +2,15 @@ from flask import Flask, request
 from flask_restful import Resource, Api
 import turnon
 import turnoff
+import time
 
 app = Flask(__name__)
 api = Api(app)
+
+class Ping(Resource):
+    def get(self):
+
+        print "[" + time.asctime(time.localtime(time.time())) + "] Pinging..."
 
 class Enable(Resource):
     def get(self):
@@ -20,6 +26,7 @@ class Disable(Resource):
 
         return 'Email automation successfully turned off.'
 
+api.add_resource(Ping, '/')
 api.add_resource(Enable, '/enable')
 api.add_resource(Disable, '/disable')
 
